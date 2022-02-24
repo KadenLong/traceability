49972208fc887627a0beb019403433c53c0e9e5e
KadenLong <kglong20@gmail.com>
2022-02-24 14:19:19 -0700

    added rollbar

--- server/server.js
+++ server/server.js
@@ -12,6 +12,16 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.css'))
 })
 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '18cd4a0683a643a3855e6c364e0924c8',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 const port = process.env.PORT || 4005
 
 app.listen(port, () => console.log(`Crip walkin in on port ${port}`))
